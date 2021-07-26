82b1831701cb0d17e6b3a4f632665f0152dc4910
Helen L <45009654+htslin103@users.noreply.github.com>
2021-07-25 23:20:26 -0600

    Created the first viewmodel and view
    Installed Caliburn Micro, set up the project with a view and view model

---
+++ HelenWpfApp/HelenWpfApp/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using HelenWpfApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelenWpfApp
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+
+        }
+
+        //Start app from here, override bootstrapperbase method and run our code 
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+        }
+    }
+}
